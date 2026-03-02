bee7b45167967b0918125fb9b2c6a67c3dc8cf1c
j-cmar <jasonmar2004.2@gmail.com>
2026-03-01 17:25:05 -0800

    fix workflow

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-cd ~SportsPrediction/frontend
+# Navigate to project directory and pull latest code
+cd ~/SportsPrediction
 git pull origin main
+
+# Deploy frontend
+cd ~/SportsPrediction/frontend
 npm install
 npm run build
 pm2 restart sportsbet
